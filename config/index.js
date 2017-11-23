bbf5138f5538183c4e21ea2ab0374fa0f77616b7
Midhet <djulovic_m@hotmail.com>
2017-11-23 14:37:16 +0100

    Refactor config/index.js file

---
+++ config/index.js
@@ -0,0 +1,23 @@
+import { request } from "https";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+const env = process.env.NODE_ENV;
+let envConfig = {};
+switch (env) {
+  case "development":
+  case "dev":
+    envConfig = require("./dev");
+    break;
+  case "testing":
+  case "test":
+    envConfig = require("./test");
+    break;
+  case "production":
+  case "prod":
+    envConfig = require("./prod");
+
+    break;
+}
+
+module.exports = envConfig;
